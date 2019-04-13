1f5afb67fad53990392b649aeafa4603e988d25f
Sachira Chinthana Jayasanka <sachira@LIVE.COM>
2019-04-13 11:57:35 +0530

    App add-in project added

---
+++ SharePoint.Authentication.Sample.AppWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SharePoint.Authentication.Sample.AppWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SharePoint.Authentication.Sample.AppWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SharePoint.Authentication.Sample.AppWeb.MvcApplication" Language="C#" %>
