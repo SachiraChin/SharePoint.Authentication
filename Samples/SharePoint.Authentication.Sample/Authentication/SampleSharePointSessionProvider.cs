b44be18e8c05d8052356740e4746829c2e0682bd
Sachira Chinthana Jayasanka <sachira@live.com>
2019-04-16 19:53:14 +0530

    Code updated for VS2017 compatibily

--- Samples/SharePoint.Authentication.Sample/Authentication/SampleSharePointSessionProvider.cs
+++ Samples/SharePoint.Authentication.Sample/Authentication/SampleSharePointSessionProvider.cs
@@ -19,71 +19,81 @@ namespace SharePoint.Authentication.Sample.Authentication
 
         public async Task SaveSharePointSession(Guid sessionId, SharePointSession sharePointSession)
         {
-            using var context = new SampleDataContext();
-            var model = new SampleSharePointSession()
+            using (var context = new SampleDataContext())
             {
-                SessionId = sessionId,
-                ContextToken = string.IsNullOrWhiteSpace(sharePointSession.ContextToken) ? null : StringCipher.Encrypt(sharePointSession.ContextToken, VerySecurePassword),
-                ContextTokenAuthority = sharePointSession.ContextTokenAuthority,
-                SharePointAppWebUrl = sharePointSession.SharePointAppWebUrl,
-                SharePointHostWebUrl = sharePointSession.SharePointHostWebUrl,
-            };
-            context.SampleSharePointSessions.Add(model);
-            await context.SaveChangesAsync();
+                var model = new SampleSharePointSession()
+                {
+                    SessionId = sessionId,
+                    ContextToken = string.IsNullOrWhiteSpace(sharePointSession.ContextToken) ? null : StringCipher.Encrypt(sharePointSession.ContextToken, VerySecurePassword),
+                    ContextTokenAuthority = sharePointSession.ContextTokenAuthority,
+                    SharePointAppWebUrl = sharePointSession.SharePointAppWebUrl,
+                    SharePointHostWebUrl = sharePointSession.SharePointHostWebUrl,
+                };
+                context.SampleSharePointSessions.Add(model);
+                await context.SaveChangesAsync();
+            }
         }
 
         public async Task<SharePointSession> GetSharePointSession(Guid sessionId)
         {
-            using var context = new SampleDataContext();
-            var dbModel = await context.SampleSharePointSessions.FirstOrDefaultAsync(s => s.SessionId == sessionId);
-            if (dbModel == null) return null;
-            var model = new SharePointSession()
+            using (var context = new SampleDataContext())
             {
-                SessionId = sessionId,
-                ContextToken = dbModel.ContextToken == null ? null : StringCipher.Decrypt(dbModel.ContextToken, VerySecurePassword),
-                ContextTokenAuthority = dbModel.ContextTokenAuthority,
-                SharePointAppWebUrl = dbModel.SharePointAppWebUrl,
-                SharePointHostWebUrl = dbModel.SharePointHostWebUrl,
-            };
+                var dbModel = await context.SampleSharePointSessions.FirstOrDefaultAsync(s => s.SessionId == sessionId);
+                if (dbModel == null) return null;
+                var model = new SharePointSession()
+                {
+                    SessionId = sessionId,
+                    ContextToken = dbModel.ContextToken == null ? null : StringCipher.Decrypt(dbModel.ContextToken, VerySecurePassword),
+                    ContextTokenAuthority = dbModel.ContextTokenAuthority,
+                    SharePointAppWebUrl = dbModel.SharePointAppWebUrl,
+                    SharePointHostWebUrl = dbModel.SharePointHostWebUrl,
+                };
 
-            return model;
+                return model;
+            }
         }
 
         public async Task SaveHighTrustCredentials(HighTrustCredentials highTrustCredentials)
         {
-            using var context = new SampleDataContext();
-            var model = new SampleHighTrustCredentials()
+            using (var context = new SampleDataContext())
             {
-                ClientId = string.IsNullOrWhiteSpace(highTrustCredentials.ClientId) ? null : StringCipher.Encrypt(highTrustCredentials.ClientId, VerySecurePassword),
-                ClientSecret = string.IsNullOrWhiteSpace(highTrustCredentials.ClientSecret) ? null : StringCipher.Encrypt(highTrustCredentials.ClientSecret, VerySecurePassword),
-                SharePointHostWebUrl = highTrustCredentials.SharePointHostWebUrl,
-                SharePointHostWebUrlHash = GetSha256(highTrustCredentials.SharePointHostWebUrl),
-            };
+                var model = new SampleHighTrustCredentials()
+                {
+                    ClientId = string.IsNullOrWhiteSpace(highTrustCredentials.ClientId) ? null : StringCipher.Encrypt(highTrustCredentials.ClientId, VerySecurePassword),
+                    ClientSecret = string.IsNullOrWhiteSpace(highTrustCredentials.ClientSecret) ? null : StringCipher.Encrypt(highTrustCredentials.ClientSecret, VerySecurePassword),
+                    SharePointHostWebUrl = highTrustCredentials.SharePointHostWebUrl,
+                    SharePointHostWebUrlHash = GetSha256(highTrustCredentials.SharePointHostWebUrl),
+                };
 
-            context.SampleHighTrustCredentials.Add(model);
-            await context.SaveChangesAsync();
+                context.SampleHighTrustCredentials.Add(model);
+                await context.SaveChangesAsync();
+            }
         }
 
         public async Task<HighTrustCredentials> GetHighTrustCredentials(string spHostWebUrl)
         {
-            using var context = new SampleDataContext();
-            var spHostWebUrlHash = GetSha256(spHostWebUrl);
-            var dbModel = await context.SampleHighTrustCredentials.FirstOrDefaultAsync(c => c.SharePointHostWebUrlHash == spHostWebUrlHash);
-            if (dbModel == null) return null;
-
-            return new HighTrustCredentials()
+            using (var context = new SampleDataContext())
             {
-                ClientId = dbModel.ClientId == null ? null : StringCipher.Decrypt(dbModel.ClientId, VerySecurePassword),
-                ClientSecret = dbModel.ClientSecret == null ? null : StringCipher.Decrypt(dbModel.ClientSecret, VerySecurePassword),
-                SharePointHostWebUrl = dbModel.SharePointHostWebUrl,
-            };
+                var spHostWebUrlHash = GetSha256(spHostWebUrl);
+                var dbModel = await context.SampleHighTrustCredentials.FirstOrDefaultAsync(c => c.SharePointHostWebUrlHash == spHostWebUrlHash);
+                if (dbModel == null) return null;
+
+                return new HighTrustCredentials()
+                {
+                    ClientId = dbModel.ClientId == null ? null : StringCipher.Decrypt(dbModel.ClientId, VerySecurePassword),
+                    ClientSecret = dbModel.ClientSecret == null ? null : StringCipher.Decrypt(dbModel.ClientSecret, VerySecurePassword),
+                    SharePointHostWebUrl = dbModel.SharePointHostWebUrl,
+                };
+            }
         }
 
         public static string GetSha256(string str)
         {
-            using var crypt = new SHA256Managed();
-            var crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(str));
-            return Convert.ToBase64String(crypto);
+            using (var crypt = new SHA256Managed())
+            {
+                var crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(str));
+                return Convert.ToBase64String(crypto);
+            }
         }
     }
 }
